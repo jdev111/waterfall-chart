04b7fe880913b9ac98771a79824394b27b6c73b7
Dillan Devram <dillandevram@Dillans-MacBook-Pro.local>
2025-11-27 15:47:43 +0000

    Add GitHub Actions workflow and update base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/inspektor-server/',
+  base: process.env.VITE_BASE_PATH || '/waterfall-chart/',
 });
